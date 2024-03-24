36ecc5fdba0d6073833d1c2118dc9d4932fd9912
babi <cdpkct@gmail.com>
2024-03-24 18:33:59 +0900

    init commit

---
+++ src/main/java/com/babi/oneDayStudy/OneDayStudyApplication.java
@@ -0,0 +1,13 @@
+package com.babi.oneDayStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneDayStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneDayStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/babi/oneDayStudy/OneDayStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babi.oneDayStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneDayStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
